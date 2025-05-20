9adbaa8f178073f8678f39bb723f5b424311236b
Rodolfo wsl <rndcsjj@gmail.com>
2025-05-19 21:29:18 -0300

    feat: implement cycles context and countdown functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './router'
 import { BrowserRouter } from 'react-router-dom'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
+      <CyclesContextProvider>
         <Router />
+      </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
